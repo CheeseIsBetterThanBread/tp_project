ceef450dade482fbd5e4737b3ac5559e405a570c
Михаил <kim.ms@phystech.edu>
2024-05-06 15:30:21 +0300

    fix minor problems

--- include/connection/ObserverSystem.h
+++ include/connection/ObserverSystem.h
@@ -2,6 +2,8 @@
 
 #include <memory>
 
+#include "../Battlefield.h"
+
 class Observable;
 
 class Observer {
--- include/connection/RemoteServer.h
+++ include/connection/RemoteServer.h
@@ -6,7 +6,7 @@ class RemoteServer : public Observable {
   // plug for now, didn't figure out how to work with sockets
  public:
   RemoteServer();
-  ~RemoteServer();
+  virtual ~RemoteServer();
 
   void send_layout();
   void receive_data();
