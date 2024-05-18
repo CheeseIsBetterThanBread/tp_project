6319d9799557bc6b27d42f6b16cb15b23ff61cb8
Михаил <kim.ms@phystech.edu>
2024-05-18 16:17:03 +0300

    remove comment

--- include/connection/RemoteServer.h
+++ include/connection/RemoteServer.h
@@ -3,7 +3,6 @@
 #include "ObserverSystem.h"
 
 class RemoteServer : public Observable {
-  // plug for now, didn't figure out how to work with sockets
  public:
   RemoteServer();
   virtual ~RemoteServer();
